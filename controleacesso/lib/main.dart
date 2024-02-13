61c020d6e9b87453e8cb74344d4ecdf674840847
draicor87 <foursquarevideomaker@gmail.com>
2024-02-13 19:51:12 -0300

    Iniciando estilização do App

--- controleacesso/lib/main.dart
+++ controleacesso/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         title: 'Controle de Acesso - Visitantes',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Color.fromRGBO(0, 115, 255, 0.571)),
         ),
         home: MyHomePage(),
       ),
@@ -38,20 +38,46 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
       body: Column(
         children: [
-          Text('A random AWESOME idea:'),
-          Text(appState.current.asLowerCase),
+          Text('A random AWESOME idea:'),         
+          BigCard(pair: pair),
 
           ElevatedButton(
             onPressed: () {
-              print('Button pressed');
+              appState.getNext();
             },
             child: Text('Next'),
             )
         ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,      
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(pair.asLowerCase, style: style),
       ),
     );
   }
